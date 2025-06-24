c16ad713f8bd5562b1764936295b6121b3bddbf1
Jose Edson Jr <edsonjunior9701@outlook.com>
2025-06-23 23:47:55 -0300

    migration para inserir produtos

---
+++ VShop.ProductAPI/Migrations/20250624023614_SeedProducts.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VShop.ProductAPI.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Products(Name, Price, Description, Stock, ImagemURL, CategoryId)" +
+                "Values('Caderno', 7.55, 'Caderno Espiral', 10, 'caderno1.jpg', 1)");
+
+            migrationBuilder.Sql("Insert into Products(Name, Price, Description, Stock, ImagemURL, CategoryId)" +
+                "Values('Lápis', 3.45, 'Lápis Preto', 20, 'lapis1.jpg', 1)");
+
+            migrationBuilder.Sql("Insert into Products(Name, Price, Description, Stock, ImagemURL, CategoryId)" +
+                "Values('Clips', 5.33, 'Clips para papel', 50, 'clips1.jpg', 2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from Products");
+        }
+    }
+}
