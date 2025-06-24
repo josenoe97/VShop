b8f6335e6803649cd0ec972983bacd58e57c64dc
Jose Edson Jr <edsonjunior9701@outlook.com>
2025-06-23 22:54:47 -0300

    feat: configuracao de contexto/databaseconnection/entityframework entre outros

---
+++ VShop.ProductAPI/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VShop.ProductAPI.Models;
+
+namespace VShop.ProductAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        //Fluent API
+    }
+}
