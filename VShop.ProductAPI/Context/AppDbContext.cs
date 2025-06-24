433c07e5a35403372be4e971a5e642e7c0c56112
Jose Edson Jr <edsonjunior9701@outlook.com>
2025-06-23 23:30:50 -0300

    sobreescrevendo as convenções do EF core com primeira migration

--- VShop.ProductAPI/Context/AppDbContext.cs
+++ VShop.ProductAPI/Context/AppDbContext.cs
@@ -10,6 +10,60 @@ namespace VShop.ProductAPI.Context
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
 
-        //Fluent API
+        //Fluent API => sobreescrita para não vir a sobrecarga padrão do entityFramework
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //category
+            modelBuilder.Entity<Category>().HasKey(c => c.CategoryId);
+
+            modelBuilder.Entity<Category>().
+                Property(c => c.Name).
+                    HasMaxLength(100).
+                        IsRequired();
+
+            //product
+            modelBuilder.Entity<Product>().
+                Property(c => c.Name).
+                    HasMaxLength(100).
+                        IsRequired();
+
+            modelBuilder.Entity<Product>().
+               Property(c => c.Description).
+                   HasMaxLength(255).
+                       IsRequired();
+
+            modelBuilder.Entity<Product>().
+               Property(c => c.ImagemURL).
+                   HasMaxLength(255).
+                       IsRequired();
+
+            modelBuilder.Entity<Product>().
+               Property(c => c.Price).
+                   HasPrecision(12, 2);
+
+            //1:N
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Products)
+                    .WithOne(c => c.Category)
+                        .IsRequired()
+                            .OnDelete(DeleteBehavior.Cascade); // DeleteBehavior.Cascade => na exclusão de uma categoria os produtos relacionados irão ser excluidos
+
+
+            //HasData => se não houver dados iniciais irá inserir
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    CategoryId = 1,
+                    Name = "Material Escolar",
+                },
+
+                new Category
+                {
+                    CategoryId = 2,
+                    Name = "Acessórios"
+                }
+            );
+        }
+
     }
 }
